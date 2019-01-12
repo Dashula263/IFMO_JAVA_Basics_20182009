c3dfa453b6d1efa0bc9ad9a5ddd11761bc6290c9
Dasha <usacheva-darya@mail.ru>
2019-01-12 11:28:12 +0500

    sad_hello3(((

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,21 +1,61 @@
 package ru.ifmo.cet.javabasics;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
+import static java.nio.file.Files.readAllLines;
 
 public class WarAndPeaceExercise {
 
-    public static String warAndPeace() {
+    private static class Node{
+
+        String key;
+        Integer value;
+
+        public Node(String key, Integer value) {
+            this.key = key;
+            this.value = value;
+        }
+
+    }
+
+    public static String warAndPeace() throws IOException {
+
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
-
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO Iff word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order iff needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
-
-        throw new UnsupportedOperationException();
+        final Charset charset = Charset.forName("windows-1251");
+        HashMap<String, Integer> map = new HashMap<>();
+        Charset set = Charset.forName("windows-1251");
+        List<String> lines = readAllLines(tome12Path, set);
+        lines.addAll(readAllLines(tome34Path, set));
+        String line = lines.toString();
+        line = line.replaceAll("[^a-zA-Zа-яА-Я]", " ").toLowerCase();
+        Stream.of(line.split(" "))
+                .map(String::toString)
+                .filter(word -> word.length() >= 4)
+                .forEach(word -> map.put(word, map.getOrDefault(word, 0) + 1));
+        ArrayList<Node> nodes = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : map.entrySet()){
+            nodes.add(new Node(entry.getKey(), entry.getValue()));
+        }
+        StringBuilder res = new StringBuilder();
+        nodes.sort((o1, o2) -> {
+            if (o1.value.equals(o2.value)) return o1.key.compareTo(o2.key);
+            return o2.value.compareTo(o1.value);
+        });
+        for (Node node : nodes){
+            if(node.value >= 10) {
+                res.append(node.key).append(" - ").append(node.value).append("\n");
+            }
+        }
+        return res.toString().substring(0, res.length()-1);
     }
 
 }
